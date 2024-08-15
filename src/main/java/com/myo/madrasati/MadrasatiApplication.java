853c809a2291b9985fdbb3414404323951e7ea87
Mouss <myousfi.services@gmail.com>
2024-08-15 12:36:49 +0200

    Initial Commit

---
+++ src/main/java/com/myo/madrasati/MadrasatiApplication.java
@@ -0,0 +1,13 @@
+package com.myo.madrasati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrasatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadrasatiApplication.class, args);
+	}
+
+}
