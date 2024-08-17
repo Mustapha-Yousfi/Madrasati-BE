dfbf7662410d61a15a65f83431dfe3e9658beba0
Mouss <myousfi.services@gmail.com>
2024-08-17 18:59:58 +0200

    Add data.sql initialization script

--- src/main/java/com/myo/madrasati/model/Student.java
+++ src/main/java/com/myo/madrasati/model/Student.java
@@ -1,24 +1,35 @@
 package com.myo.madrasati.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.util.Date;
 
 @Entity
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstName;
     private String lastName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date birthDate;
 
     public Student() {
     }
 
-    public Student(int id, String firstName, String lastName) {
+    public Student(int id, String firstName, String lastName, Date birthDate) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthDate = birthDate;
     }
+
+
 
     public int getId() {
         return id;
@@ -42,5 +53,23 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
     }
 }
